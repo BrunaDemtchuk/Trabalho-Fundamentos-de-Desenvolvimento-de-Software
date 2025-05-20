d2075283e918d9f0beae0eb38b2898a7138a0cb2
Bruna Demtchuk Galves <98340715+BrunaDemtchuk@users.noreply.github.com>
2025-05-19 22:59:02 -0300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-let botao = document.getElementById("botao");
-let mensagem = document.getElementById("mensagem");
-
-botao.onclick= msg();
-
-function msg(){
-    mensagem.innerHTML = "Seja bem-vindo à minha página!";
-}
